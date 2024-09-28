0149edf81ca305e0a03eddfb8955179ec63461f8
Azadcelik <azadcelikk12@gmail.com>
2024-09-28 16:42:09 -0400

    COllection, List in java

--- Algorithms/coll.java
+++ Algorithms/coll.java
@@ -2,6 +2,32 @@ package Algorithms;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
+
+class Teams 
+{
+    String name;
+    int stars;
+
+
+    @Override
+    public String toString()
+    {
+        return name + "="+ stars;
+    }
+
+
+    public Teams(String name,int stars)
+    {
+        this.name = name;
+        this.stars = stars;
+    }
+
+
+
+
+}
+
 
 class Lesson11 {
     
@@ -19,6 +45,30 @@ class Lesson11 {
         int numOfCollection = nums.size();
         System.out.println(numOfCollection + " space " + nums);
 
+
+
+        List<Integer> numbers = new ArrayList<>();
+
+        numbers.add(2);
+        numbers.add(4);
+        numbers.add(1233);
+        int returnedNum = numbers.get(2);
+        System.out.println(returnedNum);
+
+
+        List<Teams> footballClub = new ArrayList<>();
+
+        footballClub.add(new Teams("Real Madrid", 10));
+        footballClub.add(new Teams("Milan", 11));
+        footballClub.add(new Teams("Barcelona", 12));
+        footballClub.add(new Teams("Manchester United", 14));
+
+        System.out.println(footballClub);
+
+        for (Teams club : footballClub)
+        {
+            System.out.println(club);
+        }
 
         
     }
