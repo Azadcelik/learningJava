a41310d445a94377d5e27e3f50c02864e05897c1
Azadcelik <azadcelikk12@gmail.com>
2024-09-28 13:54:23 -0400

    jaa error

--- JavaError/JavaError.java
+++ JavaError/JavaError.java
@@ -1,9 +1,40 @@
 package JavaError;
+
+
+class AzadException extends Exception
+{
+    public AzadException(String string)
+    {
+        super(string);
+    }
+}
+
 class JavaErrorLesson {
 
 
+
+ 
+
     public static void main(String s[])
     {
+
+        String names = "Azad";
+        char myInitial = 'A';
+        int initialNumber = 9;
+        
+
+        try
+        {
+
+        if (names == "Azad")
+                throw new AzadException("My name used double time");
+        }catch(AzadException a)
+        {
+            System.out.println(a);
+        }
+
+
+
         int a = 1;
         int[] intArr = new int[8];
 
@@ -28,6 +59,8 @@ class JavaErrorLesson {
             System.out.println("this is out of bound exception error lol");
 
         }
+
+         
 
     }
 }
