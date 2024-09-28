b9225fbe08a879185a5986c77427be016e5e8bb6
Azadcelik <azadcelikk12@gmail.com>
2024-09-28 14:33:55 -0400

    collection in java

---
+++ Algorithms/coll.java
@@ -0,0 +1,25 @@
+package Algorithms;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+class Lesson11 {
+    
+
+
+
+    @SuppressWarnings("unchecked")
+    public static void main(String azad[])
+    {
+        Collection nums = new ArrayList<>();
+
+        nums.add(2);
+        nums.add(5);
+        nums.remove(2);
+        int numOfCollection = nums.size();
+        System.out.println(numOfCollection + " space " + nums);
+
+
+        
+    }
+}
